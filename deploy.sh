6e6d6f3aa4354be0a52406eb9c3d508834ca607e
naotaka nakane <n.nakane0219@gmail.com>
2019-03-25 02:04:42 +0900

    Update tmux status bar

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ ln -sfn $PWD/.vimrc ~/.vimrc
 ln -sfn $PWD/.zshenv ~/.zshenv
 ln -sfn $PWD/.zshrc ~/.zshrc
 ln -sfn $PWD/.zsh ~/.zsh
+ln -sfn $PWD/.tmux ~/.tmux
 ln -sfn $PWD/.tmux.conf ~/.tmux.conf
 ln -sfn $PWD/.gitconfig ~/.gitconfig
 ln -sfn $PWD/.gitignore_global ~/.gitignore_global
