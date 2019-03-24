2eadf5a03028ad4ddad76dc26da49bae44a96e15
naotaka nakane <n.nakane0219@gmail.com>
2019-03-24 21:42:38 +0900

    Add git files

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@
 ln -sf $PWD/.vimrc ~/.vimrc
 ln -sf $PWD/.zshrc ~/.zshrc
 ln -sf $PWD/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.gitignore_global ~/.gitignore_global
 
